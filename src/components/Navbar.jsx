44884686ce6fb8a4d5dd32455779189dedd38179
gdrmvrm <gdrmvrm@gmail.com>
2025-11-28 10:57:11 +0000

    render navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 
 import React from 'react';
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
   return (
@@ -25,12 +26,14 @@ const Navbar = () => {
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'
             >
-              nav links
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className='navbar-center hidden lg:flex'>
-          <ul className='menu menu-horizontal '>nav links</ul>
+          <ul className='menu menu-horizontal '>
+            <NavLinks />
+          </ul>
         </div>
         <div className='navbar-end'>
           {/* THEME ICONS */}
